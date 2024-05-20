48821363b799e94dbbfcf0ee9a7b1dd9ffc1461e
Sahil Kumar <sahilku601@gmail.com>
2024-05-20 11:43:57 +0530

    Navbar and Auth

---
+++ components/navbar.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import MaxWrapper from "./MaxWrapper";
+import { buttonVariants } from "./ui/button";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { ArrowBigRightIcon } from "lucide-react";
+const Navbar=()=>{
+    return(
+        <nav className="sticky inset-x-0 h-15 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+            <MaxWrapper>
+                <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+                    <Link href='/' className="flex z-40 font-semibold">
+                        <span>PDFPal.</span>
+                    </Link>
+                    <div className="hidden items-center space-x-4 sm:flex">
+                        <>
+                            <Link href='/pricing' className={buttonVariants({
+                                variant:'ghost',
+                                size:'sm',
+                            })}>
+                                Pricing
+                            </Link>
+                            <LoginLink className={buttonVariants({
+                                variant:'ghost',
+                                size:'sm',
+                            
+                            })}> Log In</LoginLink>
+                            <RegisterLink className={buttonVariants({
+                                size:'sm',
+                                
+                            })}> Sign Up <ArrowBigRightIcon/></RegisterLink>
+                        </>
+                    </div>
+                </div>
+            </MaxWrapper>
+        </nav>
+    )
+}
+
+export default Navbar;
